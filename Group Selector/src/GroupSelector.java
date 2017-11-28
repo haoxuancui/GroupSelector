5ce7d937d19049a703b38c91c0c2f14320ce8bcc
dimitris.flores@mullenmail.com <Dimitris.Flores@MU-C1-10.mullen.pvt.k12.co.us>
2017-11-28 15:01:53 -0700

    make a alhpabetical sort for each seperate array

--- Group Selector/src/GroupSelector.java
+++ Group Selector/src/GroupSelector.java
@@ -13,19 +13,22 @@ public class GroupSelector
 
 	private static void makeRoster()
 		{
-		// TODO Auto-generated method stub
 		
 		}
 
 	private static void makeGroups()
 		{
-		// TODO Auto-generated method stub
-		
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Hello how many groups would you like?");
+		int name = userInput.nextInt();
+		for(int i = 1; i <= name; i++)
+			{
+				System.out.println(i);
+			}
 		}
 
 	private static void sortGroupsAlphabetically()
 		{
-		// TODO Auto-generated method stub
 		
 		}
 
