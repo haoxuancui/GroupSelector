f2d33ed55bacd961ef0a2833bfd5fedf66a85f60
Chase Sieradski <cmsieradski@gmail.com>
2017-11-28 14:33:55 -0700

    Added methods for everyone to work on.

--- Group Selector/src/GroupSelector.java
+++ Group Selector/src/GroupSelector.java
@@ -5,8 +5,34 @@ public class GroupSelector
 
 	public static void main(String[] args)
 		{
-		// TODO Auto-generated method stub
+		makeRoster();
+		makeGroups();
+		sortGroupsAlphabetically();
+		printGroups();
+		}
 
+	private static void makeRoster()
+		{
+		// TODO Auto-generated method stub
+		
+		}
+
+	private static void makeGroups()
+		{
+		// TODO Auto-generated method stub
+		
+		}
+
+	private static void sortGroupsAlphabetically()
+		{
+		// TODO Auto-generated method stub
+		
+		}
+
+	private static void printGroups()
+		{
+		// TODO Auto-generated method stub
+		
 		}
 
 	}
