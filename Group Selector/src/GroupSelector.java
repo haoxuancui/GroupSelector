483eddba049c31e7a4b6d9f9f2b9245596b33089
haoxuancui <haoxuancui@192.168.8.39>
2017-11-30 10:32:00 -0700

    second

--- Group Selector/src/GroupSelector.java
+++ Group Selector/src/GroupSelector.java
@@ -2,11 +2,47 @@ import java.util.*;
 import java.io.*;
 public class GroupSelector
 	{
+		public static void main(String[] args)
+				{
+				makeRoster();
+				makeGroups();
+				sortGroupsAlphabetically();
+				printGroups();
+				}
 
-	public static void main(String[] args)
-		{
-		// TODO Auto-generated method stub
+			private static void makeRoster()
+				{
+				
+				}
+
+			private static void makeGroups()
+				{
+				Scanner userInput = new Scanner(System.in);
+				System.out.println("Hello how many groups would you like?");
+				int name = userInput.nextInt();
+				for(int i = 1; i <= name; i++)
+					{
+						System.out.println(i);
+					}
+				}
+
+			private static void sortGroupsAlphabetically()
+				{
+				for(int i = 0; i < groups.size(); i++)
+					{
+						groups.get(i).getLastName()
+						
+						
+						Collections.sort(groups.get(i));
+					}
+				}
+
+			private static void printGroups()
+				{
+				// TODO Auto-generated method stub
+				
+				}
 
 		}
 
-	}
+	
